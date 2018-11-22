349b68aea2639a624d4ecee4de06c26973795df0
Ryuuz <cmodegaard@gmail.com>
2018-11-22 21:24:01 +0100

    Added more components
    Did some more work on player movement.
    Added all the objects the prototype should contain.
    Made a billboard script.

---
+++ Assets/Scripts/BillboardScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardScript : MonoBehaviour {
+    public Camera playerCam;
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.LookAt(transform.position + playerCam.transform.rotation * Vector3.forward, playerCam.transform.rotation * Vector3.forward);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float walkSpeed = 10.0f;
+    public float walkSpeed = 300.0f;
+    public float turnSpeed = 0.5f;
     private Vector3 direction;
     private Vector2 rotation;
     private Rigidbody physicsBody = null;
@@ -15,13 +16,20 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 characterRotation = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-        rotation.x = rotation.x + characterRotation.x;
-        transform.rotation = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        
+    }
 
-        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        direction = transform.TransformDirection(direction) * walkSpeed;
+    private void FixedUpdate()
+    {
+        Vector2 characterRotation = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        rotation.x += characterRotation.x;
+        transform.rotation = Quaternion.AngleAxis(rotation.x * turnSpeed, Vector3.up);
+
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        direction = transform.TransformDirection(direction);
+        direction *= Time.deltaTime * walkSpeed;
         direction.y = physicsBody.velocity.y;
+        
         physicsBody.velocity = direction;
     }
 }
