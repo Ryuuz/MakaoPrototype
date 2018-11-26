588c4c40d1c0ed9005f02a753ca248cbc159c8cf
Ryuuz <cmodegaard@gmail.com>
2018-11-26 06:38:08 +0100

    Added jumping and sprinting
    The player can jump with space.
    It is now possible to sprint by holding left shift.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float walkSpeed = 300.0f;
+    public float runSpeed = 600.0f;
     public float turnSpeed = 2.0f;
     private Vector3 direction;
     private Vector2 rotation;
@@ -13,24 +14,50 @@ public class PlayerController : MonoBehaviour {
 
     private bool hasMap = false;
     private bool paused = false;
+    private bool grounded = false;
+    private float distanceToGround;
+    private float movementSpeed;
 
 	// Use this for initialization
 	void Start () {
         physicsBody = gameObject.GetComponent<Rigidbody>();
         rotation.x = gameObject.transform.rotation.eulerAngles.y;
         map = gameObject.transform.Find("MapInterface").gameObject;
+        distanceToGround = (gameObject.GetComponent<CapsuleCollider>().height / 2.0f) + 0.1f;
+        movementSpeed = walkSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(hasMap && Input.GetButtonDown("Map"))
+        if(Physics.Raycast(gameObject.transform.position, -Vector3.up, distanceToGround))
+        {
+            grounded = true;
+        }
+
+        //Respawn
+        if (gameObject.transform.position.y < -5.0f)
+        {
+            gameObject.transform.position = new Vector3(20.0f, 1.5f, 20.0f);
+        }
+
+        if (hasMap && Input.GetButtonDown("Map"))
         {
             ToggleMap();
         }
 
-        if(gameObject.transform.position.y < -5.0f)
+        if(grounded && Input.GetButtonDown("Jump"))
         {
-            gameObject.transform.position = new Vector3(20.0f, 1.5f, 20.0f);
+            physicsBody.AddForce(new Vector3(0.0f, 2000.0f, 0.0f));
+            grounded = false;
+        }
+
+        if(Input.GetButtonDown("Sprint"))
+        {
+            movementSpeed = runSpeed;
+        }
+        if(Input.GetButtonUp("Sprint"))
+        {
+            movementSpeed = walkSpeed;
         }
     }
 
@@ -42,7 +69,7 @@ public class PlayerController : MonoBehaviour {
 
         direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         direction = transform.TransformDirection(direction);
-        direction *= Time.deltaTime * walkSpeed;
+        direction *= Time.deltaTime * movementSpeed;
         direction.y = physicsBody.velocity.y;
         
         physicsBody.velocity = direction;
